6269d95df113bfb815a7a1564c5d4a9b5582ac98
Tony Kelly <Tonykelly1286@gmail.com>
2017-01-02 22:12:40 -0600

    Begining of API addition

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -118,6 +118,9 @@ main_page_content = '''
     <div class="container">
       {movie_tiles}
     </div>
+    <div class="container">
+      {api_movie}
+    </div>
   </body>
 </html>
 '''
@@ -125,6 +128,15 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" width="220" height="342">
+    <h2>{movie_title}</h2>
+    <p>{movie_storyline}<p>
+   
+</div>
+'''
+#movie api content
+movie_api_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
@@ -155,14 +167,36 @@ def create_movie_tiles_content(movies):
         )
     return content
 
+def create_movie_api_content(movies_api):
+    # The HTML content for this section of the page
+    api_content = ''
+    for movie in movies_api:
+        # Extract the youtube ID from the url
+        youtube_id_match = re.search(
+            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(
+            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                              else None)
 
-def open_movies_page(movies):
+        # Append the tile for the movie with its content filled in
+        api_content += movie_api_content.format(
+            movie_title=movie.title,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id,
+            movie_storyline=movie.storyline
+        )
+    return api_content
+
+
+def open_movies_page(movies, movies_api):
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
 
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+        movie_tiles=create_movie_tiles_content(movies),
+        api_movie=create_movie_api_content(movies_api))
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
--- movies_api.py
+++ movies_api.py
@@ -1,6 +1,12 @@
-#import fresh_tomatoes
+import fresh_tomatoes
 import urllib
+import urllib.request
+import json
 import MovieAPI
 
 url = "https://api.themoviedb.org/3/discover/movie?api_key=c3880d07fc7fcab7025895770629a5bb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-
+json_obj = urllib.request.urlopen(url)
+movie_json = json.JSONDecoder(json_obj)
+movie_api = json.load(movie_json)
+for apimovie in movie_api['results']:
+    print (apimovie)
