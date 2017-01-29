920e61288001a3155f1eb6896b7c79556debdbd9
Tony Kelly <Tonykelly1286@gmail.com>
2017-01-28 21:27:22 -0600

    docstring added

--- MovieClass.py
+++ MovieClass.py
@@ -1,7 +1,11 @@
 import webbrowser
-#This class sets up the movie data in the format needed to post to html
+
 class Movie():
+  """ This class sets up the movie data in the format needed to post to html""" 
+    
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """ this init takes in the movie title, the description or storyline, the movie poster 
+        and the youtube trailer url"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
